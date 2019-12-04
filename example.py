69ddffc70a0b984f6e38b08e215a25b660caa42b
Dan Allan <dallan@bnl.gov>
2019-12-04 09:27:04 -0500

    Use _create_qApp.

--- example.py
+++ example.py
@@ -1,13 +1,16 @@
-from qtpy.QtWidgets import QMainWindow
+from qtpy.QtWidgets import QMainWindow, QApplication
+assert QApplication.instance() is None
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 plt.ion()
+from matplotlib.backends.backend_qt5 import _create_qApp
 from bluesky import RunEngine
 RE = RunEngine()
 from ophyd.sim import det, motor
 from bluesky.plans import scan
 
+_create_qApp()
 from bluesky_mpl.qt.viewer import Viewer
 main_window = QMainWindow()
 viewer = Viewer()
@@ -15,4 +18,3 @@ main_window.setCentralWidget(viewer)
 main_window.show()
 
 RE(scan([det], motor, -1, 1, 10), viewer)
-
